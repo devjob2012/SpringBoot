21fe5bce6d5d9725a9a7cd3549cf092fa63dc2aa
Deven Rawat <devenrawat@metrobank.plc.uk>
2018-06-13 15:01:19 +0100

    Changed

--- SpringBootExample/src/main/java/co/uk/deven/spring/boot/controller/EmployeeRegistration.java
+++ SpringBootExample/src/main/java/co/uk/deven/spring/boot/controller/EmployeeRegistration.java
@@ -7,15 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.uk.deven.spring.boot.model.User;
+
 @RestController
 public class EmployeeRegistration {
 	@RequestMapping(method = RequestMethod.POST, value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
-	public void registerEmployee() {
-		System.out.println("Hello World!");
+	public void registerEmployee(@RequestBody User user) {
+		System.out.println("Hello World! " + user.getLastName());
 	}
 
 	@ExceptionHandler
